27b1edc6b0278bbad35fa5208f9662a7bbc262ab
talregev <tal.regev@gmail.com>
2014-11-19 04:52:20 +0200

    update buildAll.sh and createKey.sh. now it more generic. fix bug at build.gradle.

--- createKey.sh
+++ createKey.sh
@@ -2,12 +2,10 @@
 
 # from http://vardhan-justlikethat.blogspot.co.il/2012/05/android-solution-install-parse-failed.html
 
-BIN_DIR=~/rosjava/devel/share/maven/org/ros/android_core/android_sensors_driver/0.2.0/
-SENSOR_DIR=~/rosjava/src/android_core/android_sensors_driver/
-APK="android_sensors_driver-0.2.0.apk"
-KEY=${SENSOR_DIR}keys/${1}.jks
-ALIAS="org.ros.android.android_sensor_driver"
-
+PROJECT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+PROJECT_DIR=`basename $PROJECT_PATH`
+KEY=${PROJECT_PATH}/keys/${1}.jks
+ALIAS="org.ros.android.$PROJECT_DIR"
 
 if [ -z "$1" ]; then
     echo "Please enter your key name."
