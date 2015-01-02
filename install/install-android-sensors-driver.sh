9576553529a8517532f37f2496356fa6b34d98e5
talregev <tal.regev@gmail.com>
2015-01-02 22:08:51 +0200

    update android core install script

--- install/install-android-sensors-driver.sh
+++ install/install-android-sensors-driver.sh
@@ -2,12 +2,9 @@
 
 PROJECT_DIR=~/ANPL
 
-# clone android_core , android_extras
-cd ${PROJECT_DIR}/rosjava/src
-git clone -b indigo https://github.com/rosjava/android_core.git
-git clone -b indigo https://github.com/rosjava/android_extras.git
+install-android-core.sh
 
-cd android_core
+cd ${PROJECT_DIR}/rosjava/src/android_core
 echo "include \"android_sensors_driver\"">> settings.gradle
 
 #clone android_sensor_driver
