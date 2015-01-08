0759682e9d768e792364d8bf47eb610a0d09d706
talregev <tal.regev@gmail.com>
2015-01-08 13:14:50 +0200

    reformat the files with android studio. (Alt + Ctrl + Super + L on linux, Alt + Ctlr + l on windows).

--- src/org/ros/android/android_sensors_driver/MainActivity.java
+++ src/org/ros/android/android_sensors_driver/MainActivity.java
@@ -45,33 +45,31 @@ import java.net.URI;
  */
 
 
-public class MainActivity extends RosActivity
-{
-    protected NavSatFixPublisher        fix_pub;
-    protected ImuPublisher              imu_pub;
-    protected MagneticFieldPublisher    magnetic_field_pub;
-    protected FluidPressurePublisher    fluid_pressure_pub;
-    protected IlluminancePublisher      illuminance_pub;
-    protected TemperaturePublisher      temperature_pub;
+public class MainActivity extends RosActivity {
+    protected NavSatFixPublisher fix_pub;
+    protected ImuPublisher imu_pub;
+    protected MagneticFieldPublisher magnetic_field_pub;
+    protected FluidPressurePublisher fluid_pressure_pub;
+    protected IlluminancePublisher illuminance_pub;
+    protected TemperaturePublisher temperature_pub;
 
-    protected LocationManager           mLocationManager;
-    protected SensorManager             mSensorManager;
-    protected PowerManager.WakeLock     mWakeLock;
+    protected LocationManager mLocationManager;
+    protected SensorManager mSensorManager;
+    protected PowerManager.WakeLock mWakeLock;
 
 
-    public MainActivity()
-    {
+    public MainActivity() {
         super("ROS Sensors Driver", "ROS Sensors Driver");
     }
 
-    @Override @SuppressWarnings("deprecation")
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    @Override
+    @SuppressWarnings("deprecation")
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mLocationManager = (LocationManager)this.getSystemService(Context.LOCATION_SERVICE);
-        mSensorManager = (SensorManager)this.getSystemService(SENSOR_SERVICE);
+        mLocationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        mSensorManager = (SensorManager) this.getSystemService(SENSOR_SERVICE);
 
         final PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
         mWakeLock = pm.newWakeLock(PowerManager.SCREEN_DIM_WAKE_LOCK, "My Tag");
@@ -79,28 +77,25 @@ public class MainActivity extends RosActivity
     }
 
     @Override
-    protected  void onDestroy() {
+    protected void onDestroy() {
         mWakeLock.release();
         super.onDestroy();
     }
 
     @Override
-    protected void onPause()
-    {
+    protected void onPause() {
         super.onPause();
         mWakeLock.release();
     }
 
     @Override
-    protected void onResume()
-    {
+    protected void onResume() {
         super.onResume();
         mWakeLock.acquire();
     }
 
     @Override
-    protected void init(NodeMainExecutor nodeMainExecutor)
-    {
+    protected void init(NodeMainExecutor nodeMainExecutor) {
         URI masterURI = getMasterUri();
         //masterURI = URI.create("http://192.168.15.247:11311/");
         //masterURI = URI.create("http://10.0.1.157:11311/");
@@ -108,18 +103,18 @@ public class MainActivity extends RosActivity
         int currentapiVersion = android.os.Build.VERSION.SDK_INT;
 
         int sensorDelay = 10000; // 10,000 us == 100 Hz for Android 3.1 and above
-        if(currentapiVersion <= android.os.Build.VERSION_CODES.HONEYCOMB){
+        if (currentapiVersion <= android.os.Build.VERSION_CODES.HONEYCOMB) {
             sensorDelay = SensorManager.SENSOR_DELAY_UI; // 16.7Hz for older devices.  They only support enum values, not the microsecond version.
         }
 
         @SuppressWarnings("deprecation")
         int tempSensor = Sensor.TYPE_TEMPERATURE; // Older temperature
-        if(currentapiVersion >= android.os.Build.VERSION_CODES.ICE_CREAM_SANDWICH){
+        if (currentapiVersion >= android.os.Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
             tempSensor = Sensor.TYPE_AMBIENT_TEMPERATURE; // Use newer temperature if possible
         }
 
 
-        if(currentapiVersion >= android.os.Build.VERSION_CODES.GINGERBREAD){
+        if (currentapiVersion >= android.os.Build.VERSION_CODES.GINGERBREAD) {
             NodeConfiguration nodeConfiguration = NodeConfiguration.newPublic(InetAddressFactory.newNonLoopback().getHostAddress());
             nodeConfiguration.setMasterUri(masterURI);
             nodeConfiguration.setNodeName("android_sensors_driver_magnetic_field");
@@ -127,7 +122,7 @@ public class MainActivity extends RosActivity
             nodeMainExecutor.execute(this.magnetic_field_pub, nodeConfiguration);
         }
 
-        if(currentapiVersion >= android.os.Build.VERSION_CODES.GINGERBREAD){
+        if (currentapiVersion >= android.os.Build.VERSION_CODES.GINGERBREAD) {
             NodeConfiguration nodeConfiguration2 = NodeConfiguration.newPublic(InetAddressFactory.newNonLoopback().getHostAddress());
             nodeConfiguration2.setMasterUri(masterURI);
             nodeConfiguration2.setNodeName("android_sensors_driver_nav_sat_fix");
@@ -135,7 +130,7 @@ public class MainActivity extends RosActivity
             nodeMainExecutor.execute(this.fix_pub, nodeConfiguration2);
         }
 
-        if(currentapiVersion >= android.os.Build.VERSION_CODES.GINGERBREAD){
+        if (currentapiVersion >= android.os.Build.VERSION_CODES.GINGERBREAD) {
             NodeConfiguration nodeConfiguration3 = NodeConfiguration.newPublic(InetAddressFactory.newNonLoopback().getHostAddress());
             nodeConfiguration3.setMasterUri(masterURI);
             nodeConfiguration3.setNodeName("android_sensors_driver_imu");
@@ -143,7 +138,7 @@ public class MainActivity extends RosActivity
             nodeMainExecutor.execute(this.imu_pub, nodeConfiguration3);
         }
 
-        if(currentapiVersion >= android.os.Build.VERSION_CODES.GINGERBREAD){
+        if (currentapiVersion >= android.os.Build.VERSION_CODES.GINGERBREAD) {
             NodeConfiguration nodeConfiguration4 = NodeConfiguration.newPublic(InetAddressFactory.newNonLoopback().getHostAddress());
             nodeConfiguration4.setMasterUri(masterURI);
             nodeConfiguration4.setNodeName("android_sensors_driver_pressure");
@@ -151,7 +146,7 @@ public class MainActivity extends RosActivity
             nodeMainExecutor.execute(this.fluid_pressure_pub, nodeConfiguration4);
         }
 
-        if(currentapiVersion >= android.os.Build.VERSION_CODES.GINGERBREAD){
+        if (currentapiVersion >= android.os.Build.VERSION_CODES.GINGERBREAD) {
             NodeConfiguration nodeConfiguration5 = NodeConfiguration.newPublic(InetAddressFactory.newNonLoopback().getHostAddress());
             nodeConfiguration5.setMasterUri(masterURI);
             nodeConfiguration5.setNodeName("android_sensors_driver_illuminance");
@@ -159,7 +154,7 @@ public class MainActivity extends RosActivity
             nodeMainExecutor.execute(this.illuminance_pub, nodeConfiguration5);
         }
 
-        if(currentapiVersion >= android.os.Build.VERSION_CODES.GINGERBREAD){
+        if (currentapiVersion >= android.os.Build.VERSION_CODES.GINGERBREAD) {
             NodeConfiguration nodeConfiguration6 = NodeConfiguration.newPublic(InetAddressFactory.newNonLoopback().getHostAddress());
             nodeConfiguration6.setMasterUri(masterURI);
             nodeConfiguration6.setNodeName("android_sensors_driver_temperature");
@@ -177,55 +172,55 @@ public class MainActivity extends RosActivity
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if(item.getItemId() == R.id.menu_help) {
-                AlertDialog.Builder builder = new AlertDialog.Builder(this);
-                builder.setCancelable(true);
-                builder.setTitle(getResources().getString(R.string.help_title));
-                builder.setMessage(getResources().getString(R.string.help_message));
-                builder.setInverseBackgroundForced(true);
-                builder.setNegativeButton(getResources().getString(R.string.help_ok),
-                        new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                dialog.dismiss();
+        if (item.getItemId() == R.id.menu_help) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setCancelable(true);
+            builder.setTitle(getResources().getString(R.string.help_title));
+            builder.setMessage(getResources().getString(R.string.help_message));
+            builder.setInverseBackgroundForced(true);
+            builder.setNegativeButton(getResources().getString(R.string.help_ok),
+                    new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    });
+            builder.setNeutralButton(getResources().getString(R.string.help_wiki),
+                    new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            Intent i = new Intent(Intent.ACTION_VIEW);
+                            Uri u = Uri.parse("http://www.ros.org/wiki/android_sensors_driver");
+                            try {
+                                // Start the activity
+                                i.setData(u);
+                                startActivity(i);
+                            } catch (ActivityNotFoundException e) {
+                                // Raise on activity not found
+                                Toast toast = Toast.makeText(MainActivity.this, "Browser not found.", Toast.LENGTH_SHORT);
+                                toast.show();
                             }
-                        });
-                builder.setNeutralButton(getResources().getString(R.string.help_wiki),
-                        new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                Intent i = new Intent(Intent.ACTION_VIEW);
-                                Uri u = Uri.parse("http://www.ros.org/wiki/android_sensors_driver");
-                                try {
-                                    // Start the activity
-                                    i.setData(u);
-                                    startActivity(i);
-                                } catch (ActivityNotFoundException e) {
-                                    // Raise on activity not found
-                                    Toast toast = Toast.makeText(MainActivity.this, "Browser not found.", Toast.LENGTH_SHORT);
-                                    toast.show();
-                                }
+                        }
+                    });
+            builder.setPositiveButton(getResources().getString(R.string.help_report),
+                    new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            Intent i = new Intent(Intent.ACTION_VIEW);
+                            Uri u = Uri.parse("https://github.com/ros-android/android_sensors_driver/issues/new");
+                            try {
+                                // Start the activity
+                                i.setData(u);
+                                startActivity(i);
+                            } catch (ActivityNotFoundException e) {
+                                // Raise on activity not found
+                                Toast toast = Toast.makeText(MainActivity.this, "Browser not found.", Toast.LENGTH_SHORT);
+                                toast.show();
                             }
-                        });
-                builder.setPositiveButton(getResources().getString(R.string.help_report),
-                        new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                Intent i = new Intent(Intent.ACTION_VIEW);
-                                Uri u = Uri.parse("https://github.com/ros-android/android_sensors_driver/issues/new");
-                                try {
-                                    // Start the activity
-                                    i.setData(u);
-                                    startActivity(i);
-                                } catch (ActivityNotFoundException e) {
-                                    // Raise on activity not found
-                                    Toast toast = Toast.makeText(MainActivity.this, "Browser not found.", Toast.LENGTH_SHORT);
-                                    toast.show();
-                                }
-                            }
-                        });
-                AlertDialog alert = builder.create();
-                alert.show();
+                        }
+                    });
+            AlertDialog alert = builder.create();
+            alert.show();
 
         }
         return super.onOptionsItemSelected(item);
